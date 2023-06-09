1c8727a51720a1e9addbfa33e695fb5ac699fcae
Mahmoud Moustafa <mmoustafa.salama@gmail.com>
2023-06-09 21:14:06 +0300

    Add Serilog to write logs to Seq

---
+++ Configuration/Logging/RequestLoggingExtensions.cs
@@ -0,0 +1,69 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace dotnet_docker_example.Configuration.Logging;
+
+/// <summary>
+/// https://andrewlock.net/using-serilog-aspnetcore-in-asp-net-core-3-logging-the-selected-endpoint-name-with-serilog/
+/// https://andrewlock.net/using-serilog-aspnetcore-in-asp-net-core-3-excluding-health-check-endpoints-from-serilog-request-logging
+/// </summary>
+public static class RequestLoggingExtensions
+{
+    public static void UseCustomRequestLogging(this IApplicationBuilder app)
+    {
+        app.UseSerilogRequestLogging(options =>
+        {
+            options.GetLevel = GetLogEventLevel;
+            options.EnrichDiagnosticContext = EnrichFromRequest;
+        });
+    }
+
+    public static void EnrichFromRequest(IDiagnosticContext diagnosticContext, HttpContext httpContext)
+    {
+        var request = httpContext.Request;
+        diagnosticContext.Set("Host", request.Host);
+        diagnosticContext.Set("Protocol", request.Protocol);
+        diagnosticContext.Set("Scheme", request.Scheme);
+        diagnosticContext.Set("UserAgent", request.Headers["User-Agent"]);
+        diagnosticContext.Set("ClientIP", httpContext?.Connection?.RemoteIpAddress?.ToString());
+
+        diagnosticContext.Set("UserName", httpContext.User?.Identity?.Name);
+
+        if (request.QueryString.HasValue)
+        {
+            diagnosticContext.Set("QueryString", request.QueryString.Value);
+        }
+
+        diagnosticContext.Set("ContentType", httpContext.Response.ContentType);
+
+        var endpoint = httpContext.GetEndpoint();
+        if (endpoint is object)
+        {
+            diagnosticContext.Set("EndpointName", endpoint.DisplayName);
+        }
+    }
+
+    public static LogEventLevel GetLogEventLevel(HttpContext ctx, double _, Exception ex) =>
+        ex != null
+        ? LogEventLevel.Error
+        : ctx.Response.StatusCode > 499
+            ? LogEventLevel.Error
+            : ShouldEndpointExcluded(ctx) // Not an error, check if it was a health check or loggiing disabled
+                ? LogEventLevel.Verbose // Should be excluded, use Verbose for the log level to be lower than the minimum level specified in the logger configuration to be filtered
+                : LogEventLevel.Information;
+
+    private static bool ShouldEndpointExcluded(HttpContext ctx)
+    {
+        return IsHealthCheckEndpoint(ctx);
+    }
+
+    private static bool IsHealthCheckEndpoint(HttpContext ctx)
+    {
+        var endpoint = ctx.GetEndpoint();
+        if (endpoint is object)
+        {
+            return string.Equals(endpoint.DisplayName, "Health checks", StringComparison.Ordinal);
+        }
+        return false;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,66 @@
 using dotnet_docker_example.Configuration.Logging;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+var assemblyName = typeof(Program).Assembly.GetName().Name;
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .Enrich.WithMachineName()
+    .Enrich.WithProperty("Assembly", assemblyName)
+    .WriteTo.Console()
+    .WriteTo.Seq(serverUrl: "http://host.docker.internal:5341")
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Log.Information("Starting web host...");
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    //var caching = builder.Configuration.GetValue<string>("Caching");
+    //Log.ForContext("ConnectionString", connectionString)
+    //    .ForContext("Caching", caching)
+    //    .Information("Loaded the connection string form configuration");
+
+    var debugView = builder.Configuration.GetDebugView();
+    Log.ForContext("ConfigurationDebugView", debugView)
+        .Information("Configuration loaded");
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Host.UseSerilog();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseCustomRequestLogging();
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseCustomRequestLogging();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.Information("Host shut down complete");
+    Log.CloseAndFlush();
+}
